ba0a9f83216ead5ba3f280ffcc37c7335093637a
Kilian <kilian1103@hotmail.de>
2022-01-10 17:40:30 +0000

    remove exchange project details and keep basic response skeletton

--- client/index.js
+++ client/index.js
@@ -5,44 +5,23 @@ const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 const SHA256 = require('crypto-js/sha256');
 
-document.getElementById("exchange-address").addEventListener('input', ({target: {value}}) => {
-    if (value === "") {
-        document.getElementById("balance").innerHTML = 0;
-        return;
-    }
 
-    fetch(`${server}/balance/${value}`).then((response) => {
-        return response.json();
-    }).then(({balance}) => {
-        document.getElementById("balance").innerHTML = balance;
-    });
-});
-
-document.getElementById("transfer-amount").addEventListener('click', () => {
-    const sender = document.getElementById("exchange-address").value;
-    const amount = document.getElementById("send-amount").value;
-    const recipient = document.getElementById("recipient").value;
-    const privatekey = document.getElementById("privatekey").value;
-
-    // signing process
-    const key = ec.keyFromPrivate(privatekey.toString());
-
-    const message = "Send " + recipient + " " + amount + " by " + sender
-    const msgHash = SHA256(message);
-    const stringmsgHash = msgHash.toString();
-    const signature = key.sign(msgHash.toString());
+document.getElementById("send-message").addEventListener('click', () => {
+    const message = document.getElementById("message").value;
 
     const
         body = JSON.stringify({
-            sender, amount, recipient, signature, stringmsgHash
+            message
         });
+
+
 
     const request = new Request(`${server}/send`, {method: 'POST', body});
 
     fetch(request, {headers: {'Content-Type': 'application/json'}}).then(response => {
         return response.json();
-    }).then(({balance}) => {
-        document.getElementById("balance").innerHTML = balance;
+    }).then((data) => {
+        document.getElementById("response").innerHTML = data.response;
     });
 });
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3041;
+const port = 3042;
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 // localhost can have cross origin errors
@@ -9,76 +9,19 @@ const ec = new EC('secp256k1');
 app.use(cors());
 app.use(express.json());
 
-// create bank accounts
-const balances = {};
-const publicXYKeys  ={};
-const privateKeys = {};
 
-for (let i = 0;i < 3; i++){
-    const key = ec.genKeyPair();
-    const publicX = key.getPublic().x.toString(16);
-    const publicY = key.getPublic().y.toString(16);
-    const privateKey = key.getPrivate().toString(16);
-    const publicKey = key.getPublic().encode('hex')
-    balances[publicKey] = (i+1)*75;
-    publicXYKeys[publicKey] = [publicX, publicY];
-    privateKeys[publicKey] =  privateKey;
-}
-
-
-app.get('/balance/:address', (req, res) => {
-    const {address} = req.params;
-    const balance = balances[address] || 0;
-    res.send({balance});
-});
 
 app.post('/send', (req, res) => {
-    const {sender, recipient, amount, signature, stringmsgHash} = req.body;
+    const {message} = req.body;
 
+    res.send({response: ["WAS LOS AMK"]});
 
-    if (sender in balances){
-        const publicKey = {
-            x: publicXYKeys[sender][0],
-            y: publicXYKeys[sender][1]
-        }
-        const serverSignature = {
-            r: signature.r,
-            s: signature.s}
-
-        const serverkey = ec.keyFromPublic(publicKey);
-        //verify process
-        if(serverkey.verify(stringmsgHash, serverSignature)){
-            balances[sender] -= amount;
-            balances[recipient] = (balances[recipient] || 0) + +amount;
-            console.log("SUCCESS");
-            res.send({balance: balances[sender]});
-        }
-        else{
-            console.log("NO PERMISSION FOR THIS ACCOUNT")
-        }
-    }
-    else{
-        // anybody can send me money
-        if(recipient in balances){
-            balances[recipient] = (balances[recipient] || 0) + +amount;
-            console.log("SUCCESS");
-            res.send({balance: balances[sender]});
-        }
-        else{
-            console.log("SORRY NO ACCOUNT HERE")
-        }
-    }
 });
 
 app.listen(port, () => {
 
     console.log(`Listening on port ${port}!`);
-    console.log('Available accounts: ');
-    console.log(balances)
-    console.log('Private Keys');
-    for (const v of Object.values(privateKeys)){
-        console.log(v);
-    }
+
 
 });
 
