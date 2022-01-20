df7d31e3acd87bb0b743d0173ebada41375da193
Kilian <kilian1103@hotmail.de>
2022-01-20 11:06:17 +0000

    further clean up

--- server/index.js
+++ server/index.js
@@ -3,18 +3,16 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
 
-
 app.post('/send', (req, res) => {
     const {message} = req.body;
 
-    res.send({response: ["WAS LOS AMK"]});
+    res.send({response: ["HI"]});
 
 });
 
