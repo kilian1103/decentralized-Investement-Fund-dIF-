b1e9928e639a4a6b0b92b187a178953769c905b6
Kilian <kilian1103@hotmail.de>
2022-02-19 21:24:58 +0000

    clean up codebase

--- contracts/Factory.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract Factory {
-  function getPair(address tokenA, address tokenB) external view returns (address pair){}
-  
-}
--- contracts/Uniswap.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract Uniswap {
-  function getPair(address exc1, address exc2) public view returns(address out){}
-
-}
